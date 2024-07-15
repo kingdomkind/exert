561caca90d864d56ca33682fcc12c6ca0325aae7
kingdomkind <pikadevs@proton.me>
2024-07-15 17:42:10 +0100

    a

--- run.sh
+++ run.sh
@@ -5,9 +5,9 @@ set -e
 
 #> Compile & Run
 git pull &&
-cd build
-meson compile
+cd build &&
+meson compile && 
 cd ..
 
-echo "xterm & exec /home/pika/exert/build/exert" > /home/pika/.xinitrc
+echo "xterm & exec /home/pika/exert/build/exert" > /home/pika/.xinitrc &&
 startx > log.txt
