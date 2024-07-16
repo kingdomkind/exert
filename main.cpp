9cc5872f791b5329e1bfe18d4675f6780c836bc3
kingdomkind <pikadevs@proton.me>
2024-07-16 17:21:16 +0100

    Border colours seem to work

--- main.cpp
+++ main.cpp
@@ -45,11 +45,11 @@ unsigned int KeycodeToKeysym(const unsigned int Keycode) {
 
 void OnMapRequest(const xcb_generic_event_t* NextEvent) {
     xcb_map_request_event_t* Event = (xcb_map_request_event_t*)NextEvent;
-    uint32_t Parameters[] = {0, 0, 800, 800, 3};
+    uint32_t Parameters[] = {0, 0, 1280, 800, 3};
     uint32_t AttributesMasks[] = {XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_FOCUS_CHANGE | 0xff0000};
     uint32_t ConfigureMasks = XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT | XCB_CONFIG_WINDOW_BORDER_WIDTH;
 
-    xcb_change_window_attributes(WM.Connection, Event->window, XCB_CW_EVENT_MASK | XCB_CW_BORDER_PIXEL, &AttributesMasks);
+    xcb_change_window_attributes(WM.Connection, Event->window, XCB_CW_EVENT_MASK | XCB_CW_BORDER_PIXEL, &AttributesMasks); // Before this was checked, test if needed
     xcb_configure_window(WM.Connection, Event->window, ConfigureMasks, Parameters);
 
     xcb_map_window(WM.Connection, Event->window);
