041f12395d3b2cd2a4333fe2de80968bb01ebb59
kingdomkind <pikadevs@proton.me>
2024-07-16 17:19:10 +0100

    a

--- main.cpp
+++ main.cpp
@@ -46,7 +46,7 @@ unsigned int KeycodeToKeysym(const unsigned int Keycode) {
 void OnMapRequest(const xcb_generic_event_t* NextEvent) {
     xcb_map_request_event_t* Event = (xcb_map_request_event_t*)NextEvent;
     uint32_t Parameters[] = {0, 0, 800, 800, 3};
-    uint32_t AttributesMasks[] = {XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_FOCUS_CHANGE | 0xff0000};
+    uint32_t AttributesMasks[] = {XCB_EVENT_MASK_ENTER_WINDOW | XCB_EVENT_MASK_FOCUS_CHANGE | 0x0000ff};
     uint32_t ConfigureMasks = XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT | XCB_CONFIG_WINDOW_BORDER_WIDTH;
 
     xcb_change_window_attributes_checked(WM.Connection, Event->window, XCB_CW_EVENT_MASK | XCB_CW_BORDER_PIXEL, &AttributesMasks);
