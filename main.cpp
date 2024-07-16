a0f05cb6a7a02fb8290ba071c15549a7666aa07a
kingdomkind <pikadevs@proton.me>
2024-07-16 19:12:46 +0100

    a

--- main.cpp
+++ main.cpp
@@ -24,7 +24,7 @@ const uint32_t BORDER_WIDTH = 3;
 
 void KillActive() {
     std::cout << "Attempting to kill window: " << WM.InputWindow << std::endl;
-    xcb_kill_client(WM.Connection, WM.InputWindow);
+    xcb_kill_client_checked(WM.Connection, WM.InputWindow);
 }
 
 void ExitWM() {
