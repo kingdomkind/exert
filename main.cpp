2e92999b5b2894b96eb72ba7161a7ebb3a6771a8
kingdomkind <pikadevs@proton.me>
2024-07-22 10:20:07 +0100

    a

--- main.cpp
+++ main.cpp
@@ -268,7 +268,7 @@ void UpdateWindowToCurrentSplits(std::shared_ptr<Container> TargetContainer) {
 
     std::stack<std::shared_ptr<Container>> Stack;
     uint32_t X, Y, Width, Height;
-    X = 0; Y = 0; Width = 1280; Height = 800;
+    X = 0; Y = 0; Width = 3840; Height = 2160;
 
     std::shared_ptr<Container>* CurrentContainer = &TargetContainer;
     while (true) {
