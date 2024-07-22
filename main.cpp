bd9cf2c66f22cdad07221442dbc32dbb6060dbc1
kingdomkind <pikadevs@proton.me>
2024-07-22 12:19:03 +0100

    a

--- main.cpp
+++ main.cpp
@@ -555,8 +555,7 @@ void OnKeyPress(const xcb_generic_event_t* NextEvent) {
 
 void RunEventLoop() {
     std::cout << "Running the event loop" << std::endl;
-    if (fork() == 0) { std::cout << "showing kitty" << std::endl; execl("/bin/sh", "/bin/sh", "-c", "kitty", (void *)NULL);}
-    std::cout << "Past" << std::endl;
+    //if (fork() == 0) { std::cout << "showing kitty" << std::endl; execl("/bin/sh", "/bin/sh", "-c", "kitty", (void *)NULL);}
 
     while (true) {
         xcb_generic_event_t* NextEvent = xcb_wait_for_event(WM.Connection);
