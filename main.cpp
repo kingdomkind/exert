a1247a950bf1c7d657ec374c4906d99e41a3ba1d
kingdomkind <pikadevs@proton.me>
2024-07-26 23:13:44 +0100

    a

--- main.cpp
+++ main.cpp
@@ -343,6 +343,10 @@ void UpdateWindowToCurrentSplits(std::shared_ptr<Container> TargetContainer) {
     xcb_configure_window(WM.Connection, TargetContainer->Value->Window, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT | XCB_CONFIG_WINDOW_BORDER_WIDTH, Parameters);
     xcb_flush(WM.Connection);
     std::cout << "Updated Window " << TargetContainer->Value->Window << " to current splits, PosX: " << X << ", PosY: " << Y << ", Width: " << Width << ", Height: " << Height << std::endl;
+
+                uint32_t values[] = { XCB_STACK_MODE_BELOW };
+            xcb_configure_window(WM.Connection, TargetContainer->Value->Window, XCB_CONFIG_WINDOW_STACK_MODE, values);
+            xcb_flush(WM.Connection);
 }
 
 
