ed2df7b44c1c81fa12d2e95279fa6680c3b338b1
kingdomkind <pikadevs@proton.me>
2024-08-19 23:53:14 +0100

    a

--- main.cpp
+++ main.cpp
@@ -362,7 +362,7 @@ void UpdateWindowToCurrentSplits(std::shared_ptr<Container> TargetContainer) {
     }
 
     uint32_t BorderWidth = TargetContainer->Value->Floating ? Runtime.Settings.FloatingWindowBorderSize : Runtime.Settings.TiledWindowBorderSize;
-    uint32_t Parameters[] = {X, Y, Width, Height, BorderWidth};
+    uint32_t Parameters[] = {X, Y, Width-(2*BorderWidth), Height-(2*BorderWidth), BorderWidth};
     xcb_configure_window(WM.Connection, TargetContainer->Value->Window, XCB_CONFIG_WINDOW_X | XCB_CONFIG_WINDOW_Y | XCB_CONFIG_WINDOW_WIDTH | XCB_CONFIG_WINDOW_HEIGHT | XCB_CONFIG_WINDOW_BORDER_WIDTH, Parameters);
     xcb_flush(WM.Connection);
     std::cout << "Updated Window " << TargetContainer->Value->Window << " to current splits, PosX: " << X << ", PosY: " << Y << ", Width: " << Width << ", Height: " << Height << std::endl;
